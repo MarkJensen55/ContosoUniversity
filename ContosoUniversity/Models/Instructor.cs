3e8ba76bdec329a43ca75b402b44eea35a8e0f9b
MarkJensen55 <markjensen@tds.net>
2015-04-27 10:20:25 -0400

    Added instructor course relationship

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -21,14 +21,14 @@ namespace ContosoUniversity.Models
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true]
+        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
         [Display(Name="Date Hired")]
         public DateTime HireDate {get; set;}
 
         [Display(Name="Full Name")]
         public string FullName
         {
-            get {return LastName + ", " + FirstMidName}
+            get { return LastName + ", " + FirstMidName; }
         }
 
         public virtual ICollection<Course> Courses {get; set;}
